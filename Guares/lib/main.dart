cbda621e4d7da08894fb8c9e643a79f39e98aa40
AbdulRauf <mrrauf228@gmail.com>
2026-09-08 21:33:06 +0500

    main dart file had name habithub so chnaged to Guares

--- Guares/lib/main.dart
+++ Guares/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'HabitHub',
+      title: 'Guares',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF22C55E)),
       ),
